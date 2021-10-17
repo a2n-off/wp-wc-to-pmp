1911912273077ae1e4f0cd8a1ba34ea5e3d07346
a2n <bouteiller.alan@gmail.com>
2021-10-17 17:56:28 +0200

    improve user callback

--- wp-content/plugins/wc-douchette/script.js
+++ wp-content/plugins/wc-douchette/script.js
@@ -1,26 +1,29 @@
 console.log('wcdouchette js loaded');
 
 // get all the pmp buttons
-const elems = document.getElementsByClassName("pmp-sender");
+const elems = document.getElementsByClassName('pmp-sender');
 
 // init xhr function
 function sendToPmp(elem) {
   const host = window.location.origin;
   const id = elem.dataset.pmp;
   const url = host + '/wp-json/wcdouchette/v1/send/' + id;
-  console.log(url);
   const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = () => {
     // reload the page after the update
     // give the possibilty to update the data into the table
     if (xhr.readyState == XMLHttpRequest.DONE) window.location.reload(false);
   }
-  xhr.open("POST", url, true);
+  xhr.open('POST', url, true);
   xhr.send();
+  // set some style for user callback
+  elem.innerHTML = 'Processing';
+  setInterval(() => {
+    elem.style.backgroundColor = (elem.style.backgroundColor == 'rgb(198, 225, 198)' ? '' : 'rgb(198, 225, 198)');
+  }, 250);
 }
 
 // add event on each btn
 for (let i=0; i<elems.length; i++) {
-  console.log(elems[i]);
   elems[i].addEventListener('click', () => sendToPmp(elems[i]));
 }
