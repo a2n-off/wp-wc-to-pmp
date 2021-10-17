f7db91a7b08be84bc5a973eb0feceddc39217dc9
a2n <bouteiller.alan@gmail.com>
2021-10-17 17:34:58 +0200

    add trigger

--- wp-content/plugins/wc-douchette/wc-douchette-button.php
+++ wp-content/plugins/wc-douchette/wc-douchette-button.php
@@ -23,20 +23,55 @@ class WcdouchetteButton {
 	}
 
   /**
-   * create a custom button on action field in woocommerce orders table page
-   * @param $order
-   */
-  public function wcdouchette_btn_in_wcactions_column($order) {
-      $tooltip = __('Send to Mission Prepa', 'textdomain');
-      $label = __('Send to PMP', 'textdomain');
-      $id = json_decode($order)->id;
-			echo '<a href="'.get_site_url().'/wp-json/wcdouchette/v1/send/'.$id.'" type="button" class="pmp-sender button tips" style="text-indent: 0 !important; width: fit-content; padding: 0 10px !important" data-tip="'.$tooltip.'">'.$label.'</a>';
-  }
+	 * add custom colum in admin orders table
+	 */
+	function wcdouchette_add_pmp_column($columns) {
+    $new_columns = array();
+    foreach ( $columns as $column_name => $column_info ) {
+        $new_columns[ $column_name ] = $column_info;
+        if ( 'order_total' === $column_name ) $new_columns['order_details'] = __( 'PMP actions', 'my-textdomain' );
+    }
+    return $new_columns;
+	}
+
+	/**
+	 * add content into the custom column
+	 */
+	function wcdouchette_add_pmp_column_content($column) {
+		global $post;
+		if ( 'order_details' === $column ) {
+			$tooltip = __('Send to Mission Prepa', 'textdomain');
+			$label = __('Send to PMP', 'textdomain');
+			$order = wc_get_order( $post->ID );
+			$id = $order->id;
+			echo '
+			<button
+			  type="button"
+			  class="pmp-sender button tips"
+			  style="text-indent: 0 !important; width: fit-content; padding: 0 10px !important"
+			  data-tip="'.$tooltip.'"
+				data-pmp="'.$id.'"
+			>
+			  '.$label.'
+			</button>
+			';
+    }
+	}
+
+	/**
+	 * include script file
+	 * @throws Exception
+	 */
+	public function wcdouchette_includes_js() {
+		wp_enqueue_script('wcdouchette_script',  plugin_dir_url( __FILE__ ) . 'script.js', [], null, true);
+	}
 
 	/**
 	 * add action btn into the order table
 	 */
 	private function init() {
-    add_action('woocommerce_admin_order_actions_end', array($this, 'wcdouchette_btn_in_wcactions_column'));
+		add_action('admin_enqueue_scripts', array($this, 'wcdouchette_includes_js'), 100);
+		add_filter('manage_edit-shop_order_columns', array($this, 'wcdouchette_add_pmp_column'), 20);
+		add_action('manage_shop_order_posts_custom_column', array($this, 'wcdouchette_add_pmp_column_content'));
 	}
 }
