acd10e3339fbd45483d0b630262b1b53baeafdd3
a2n <bouteiller.alan@gmail.com>
2021-10-17 17:35:15 +0200

    setup the call and callback

--- wp-content/plugins/wc-douchette/wc-douchette-api.php
+++ wp-content/plugins/wc-douchette/wc-douchette-api.php
@@ -6,7 +6,8 @@ class WcdouchetteApi {
    */
   protected static $_instance = null;
 
-  protected static $_current_user = null;
+  protected static $_user = null;
+  protected static $_isDev = null;
 
   /**
    * launch init()
@@ -35,26 +36,31 @@ class WcdouchetteApi {
    * check if the user have the authorization
    */
    function wc_douchette_authguard() {
-     return user_can($this->user, 'administrator');
+     return user_can($this->_user, 'administrator');
    }
 
   /**
-   * todo function
+   * core function for the call to pmp
    */
   public function wc_douchette_send(WP_REST_Request $request) {
     $order_id = $request->get_param('id');
     $consumer_key=esc_attr(get_option('wcdouchette_option_pmp_ck'));
     $consumer_secret=esc_attr(get_option('wcdouchette_option_pmp_cs'));
-    $args = array(
-      'sslverify' => false,
+    // send to pmp
+    // todo
+    // update if success
+    $update = wp_remote_request(get_site_url().'/wp-json/wc/v3/orders/'.$order_id, array(
+      'method' => 'PUT',
       'headers' => array(
+        'Content-Type' => 'application/json',
         'Authorization' => 'Basic ' . base64_encode( $consumer_key . ':' . $consumer_secret )
-      )
-    );
-    $response = wp_remote_get(get_site_url().'/wp-json/wc/v3/orders/'.$order_id, $args);
-    $body = wp_remote_retrieve_body($response);
-    // return new WP_REST_Response( ['pong', $order_id, json_decode($body), $this->user], 200);
-    wp_redirect('https://wp-pp.test/wp-admin/edit.php?post_type=shop_order');
+      ),
+      'body' => json_encode(array("status" => "on-hold")),
+      'sslverify' => !$this->_isDev, // ONLY IN DEV
+    ));
+    $update_info = wp_remote_retrieve_body($update);
+    if (json_decode($update_info)->data->status === 401) return new WP_REST_Response('check your woocommerce key', 401);
+    return new WP_REST_Response( ['updated', $order_id], 200);
   }
 
   /**
@@ -62,7 +68,7 @@ class WcdouchetteApi {
    */
    public function wcdouchette_register_routes() {
      register_rest_route( 'wcdouchette/v1', '/send/(?P<id>\d+)', array(
-       'methods'  => 'GET',
+       'methods'  => 'POST',
        'callback' => array( $this, 'wc_douchette_send'),
        'args' => array('id' => array('validate_callback' => array($this, 'wc_douchette_param_is_numeric'))),
        'permission_callback' => array($this, 'wc_douchette_authguard'),
@@ -73,10 +79,12 @@ class WcdouchetteApi {
    * load custom api endpoint
    */
   private function init() {
+    // check env
+    if (get_site_url() === 'https://wp-pp.test') $this->_isDev = true;
     // populate user info
     global $current_user;
     get_currentuserinfo();
-    $this->user = $current_user;
+    $this->_user = $current_user;
     // add api routes
     add_action('rest_api_init', array($this, 'wcdouchette_register_routes'));
     add_filter('rest_authentication_errors', array($this, 'wcdouchette_register_routes'));
